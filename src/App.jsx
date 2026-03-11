c23225e9558a621420f88666b6f90de6eb107a04
Desiyan0504 <desiyanpec07@gmail.com>
2026-03-11 20:02:14 +0530

    First Version

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import About from "./Components/About"
+import Contact from "./Components/Contact"
+import Footer from "./Components/Footer"
+import Header from "./Components/Header"
+import Projects from "./Components/Projects"
+import Testimonials from "./Components/Testimonials"
+
+function App() {
+  return (
+    <div className="w-full overflow-hidden">
+      <Header/>
+      <About/>
+      <Projects/>
+      <Testimonials/>
+      <Contact/>
+      <Footer/>
+    </div>
+  )
+}
+
+export default App
